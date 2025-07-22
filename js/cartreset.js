21fbe73b8c8aa0c385ce8072c66c1fad1489e864
Lakshan1806 <lakshanuthayan@gmail.com>
2025-07-22 10:02:32 +0530

    feat: add styling to cart page

---
+++ js/cartreset.js
@@ -0,0 +1,20 @@
+document.getElementById("reset-cart").addEventListener("click", () => {
+  localStorage.removeItem("cart");
+
+  document.getElementById("cart-items").innerHTML =
+    '<p class="cartreset">Your cart has been reset.</p>';
+  document.getElementById("subtotal-container").innerHTML =
+    '<p class="total">Subtotal : LKR 0.00</p>';
+
+  let formattedSubtotal = new Intl.NumberFormat("en-LK", {
+    style: "currency",
+    currency: "LKR",
+    minimumFractionDigits: 2,
+  }).format(0);
+
+  document.getElementById("subtotal-container").innerHTML = `
+  <div class="subtotal-info">
+    <p class="total">Subtotal : ${formattedSubtotal}</p>
+  </div>
+`;
+});
